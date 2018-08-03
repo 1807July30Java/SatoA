2ddae76b742a71adbf0e1bdf501a248b31f6ffe7
Alan Sato <sam.alan.sato@gmail.com>
2018-08-03 10:32:44 -0400

    make the project for friday

---
+++ ScannerCalc/src/com/revature/calc/Calculator.java
@@ -0,0 +1,138 @@
+package com.revature.calc;
+
+public class Calculator {
+	/*
+	 * INSTRUCTIONS:::::::
+	 * Build a Calculator class with the ability to add, subtract, divide, and
+	 * multiply numbers of any type (byte, short, int, long, float, double)
+	 */
+	/*
+	 * ADDITION
+	 */
+	public static byte add(byte num1, byte num2) {
+		byte answer = (byte) (num1 + num2);
+		return answer;
+	}
+
+	public static short add(short num1, short num2) {
+		short answer = (short) (num1 + num2);
+		return answer;
+	}
+
+	public static int add(int num1, int num2) {
+		int answer = num1 + num2;
+		return answer;
+	}
+
+	public static long add(long num1, long num2) {
+		long answer = num1 + num2;
+		return answer;
+	}
+
+	public static float add(float num1, float num2) {
+		float answer = num1 + num2;
+		return answer;
+	}
+
+	public static double add(double num1, double num2) {
+		double answer = num1 + num2;
+		return answer;
+	}
+	/*
+	 * SUBTRACTION!
+	 */
+	public static byte sub(byte num1, byte num2) {
+		byte answer = (byte) (num1 - num2);
+		return answer;
+	}
+
+	public static short sub(short num1, short num2) {
+		short answer = (short) (num1 - num2);
+		return answer;
+	}
+
+	public static int sub(int num1, int num2) {
+		int answer = num1 - num2;
+		return answer;
+	}
+
+	public static long sub(long num1, long num2) {
+		long answer = num1 - num2;
+		return answer;
+	}
+
+	public static float sub(float num1, float num2) {
+		float answer = num1 - num2;
+		return answer;
+	}
+
+	public static double sub(double num1, double num2) {
+		double answer = num1 - num2;
+		return answer;
+	}
+	/*
+	 * MULTIPLICATION
+	 */
+	public static byte mul(byte num1, byte num2) {
+		byte answer = (byte) (num1 * num2);
+		return answer;
+	}
+
+	public static short mul(short num1, short num2) {
+		short answer = (short) (num1 * num2);
+		return answer;
+	}
+
+	public static int mul(int num1, int num2) {
+		int answer = num1 * num2;
+		return answer;
+	}
+
+	public static long mul(long num1, long num2) {
+		long answer = num1 * num2;
+		return answer;
+	}
+
+	public static float mul(float num1, float num2) {
+		float answer = num1 * num2;
+		return answer;
+	}
+
+	public static double mul(double num1, double num2) {
+		double answer = num1 * num2;
+		return answer;
+	}
+	/*
+	 * DEVISION
+	 */
+	public static byte div(byte num1, byte num2) {
+		byte answer = (byte) (num1 / num2);
+		return answer;
+	}
+
+	public static short div(short num1, short num2) {
+		short answer = (short) (num1 / num2);
+		return answer;
+	}
+
+	public static int div(int num1, int num2) {
+		int answer = num1 / num2;
+		return answer;
+	}
+
+	public static long div(long num1, long num2) {
+		long answer = num1 / num2;
+		return answer;
+	}
+
+	public static float div(float num1, float num2) {
+		float answer = num1 / num2;
+		return answer;
+	}
+
+	public static double div(double num1, double num2) {
+		double answer = num1 / num2;
+		return answer;
+	}
+	
+}
---
+++ ScannerCalc/src/com/revature/main/Driver.java
@@ -0,0 +1,13 @@
+package com.revature.main;
+import java.util.Scanner;
+import com.revature.calc.*;
+
+public class Driver {
+	//takes input from user, and parses their query to perform the appropriate calculator operation
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		System.out.println(s);
+	}
+
+}
