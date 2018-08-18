b15dedeb1c7f7542f25d4834736de684ebf2d3dd
Alan Sato <sam.alan.sato@gmail.com>
2018-08-18 17:48:39 -0400

    flesh out interface for request DAO

--- project1Sato/src/main/java/com/revature/dao/RequestDao.java
+++ project1Sato/src/main/java/com/revature/dao/RequestDao.java
@@ -1,5 +1,19 @@
 package com.revature.dao;
 
-public interface RequestDao {
+import java.util.List;
+import com.revature.pojos.*;
 
+public interface RequestDao {
+	
+	public List<RRequest> getRequestsFromEmployee(int employeeID);
+	public List<RRequest> getRequestsFromEmployee(Employee e);
+	
+	public List<RRequest> getRequestsForManager(int managerID);
+	public List<RRequest> getRequestsForManager(Employee man);
+	
+	public boolean submitRequest(RRequest req);
+	
+	public boolean updateRequest(int reqID);
+	public boolean updateRequest(RRequest req);
+	
 }
