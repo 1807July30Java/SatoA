904418aaa376e9cc8320569e0684e5d0b73f3268
Alan Sato <sam.alan.sato@gmail.com>
2018-08-18 17:48:20 -0400

    flesh out interface for employee DAO

--- project1Sato/src/main/java/com/revature/dao/EmployeeDao.java
+++ project1Sato/src/main/java/com/revature/dao/EmployeeDao.java
@@ -1,5 +1,14 @@
 package com.revature.dao;
 
-public interface EmployeeDao {
+import java.util.List;
+import com.revature.pojos.*;
 
+public interface EmployeeDao {
+	
+	public Employee getEmployee(int employeeId);
+	public Employee getEmployee(String user, String pass);
+	
+	public Employee addEmployee(Employee e);
+	public List<Employee> getAllEmployees(int employeeId);
+	
 }
